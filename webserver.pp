64856de4acf2140293580829b3e21c71baaab620
Maten Rehimi <35535096+MatenRehimi@users.noreply.github.com>
2023-10-13 19:37:22 +0100

    Update webserver.pp

--- webserver.pp
+++ webserver.pp
@@ -1,4 +1,4 @@
 file { '/var/www/html/index.html':
-  content => "Welcome to our website! This is our message.\n  Lost makes no sense!",
+  content => "Welcome to our website! This is our message.\n  Change made!",
   ensure  => present,
 }
